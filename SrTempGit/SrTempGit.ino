c9cdcb84613649fde7a833912a4f3a5d46b3f3ce
jorgecoss <jorge.coss@ICLOUD.com>
2023-05-31 12:20:53 -0600

    funcionando lat mandando json

--- SrTempGit/SrTempGit.ino
+++ SrTempGit/SrTempGit.ino
@@ -8,14 +8,15 @@
 #include <Time.h>
 #include <TimeLib.h>
 #include <String.h>
+#include <SoftwareSerial.h>
+#include <TinyGPSPlus.h>
 
 // Data wire is plugged into port 8
 #define ONE_WIRE_BUS D9
+const int RX = 4;
+const int TX = 3;
 
-// Setup a oneWire instance to communicate with any OneWire devices
-OneWire oneWire(ONE_WIRE_BUS);
 
-DallasTemperature sensors(&oneWire);
 
 const char* ssid = "HUAWEI-B310-E360";
 const char* password = "CASACOSS21";
@@ -32,13 +33,23 @@ const char* serverName = "http://200.94.111.227:8530/API/sensor";
 unsigned long lastTime = 0;
 unsigned long timerDelay = 3000;  //30000 30 seg  Cada cuanto se va a leer el sensor 300000 5 min
 
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+TinyGPSPlus gps;
+SoftwareSerial ss(RX, TX);
+
+
 char tempReadings[1000][5];
 char hourReadings[1000][9];
 char dateReadings[1000][10];
+char latReadings[1000][10];
+char lngReadings[1000][10];
 int cantidadLecturas = 0;
 
 void setup() {
   Serial.begin(115200);
+  ss.begin(38400);
   pinMode(LED_BUILTIN, OUTPUT);
   sensors.begin();
   Serial.println("iniciando");
@@ -53,13 +64,44 @@ void loop() {
   String fecha = String(year(t)) + "-" + String(month(t)) + "-" + String(day(t));
   String hora = String(hour(t)) + ":" + String(minute(t)) + ":" + String(second(t));
 
+  while (ss.available() > 0)
+    if (gps.encode(ss.read())) {
+      Serial.print(F("Location: "));
+      if (gps.location.isValid()) {
+        dtostrf(gps.location.lat(),8,6,latReadings[cantidadLecturas]);
+      dtostrf(gps.location.lng(),9,6,lngReadings[cantidadLecturas]);
+      Serial.println(latReadings[cantidadLecturas]);
+      Serial.println(lngReadings[cantidadLecturas]);
+     
+
+      } else {
+        Serial.print(F("INVALID"));
+      }
+    }
+  if (millis() > 5000 && gps.charsProcessed() < 10) {
+    Serial.println(F("No GPS detected: check wiring."));
+    while (true)
+      ;
+  }
+
+
   if ((millis() - lastTime) >= timerDelay) {
+
     Serial.print("Requesting temperatures...");
     sensors.requestTemperatures();  // Send the command to get temperatures
     Serial.println("DONE");
-    String(sensors.getTempCByIndex(0), 2).toCharArray(tempReadings[cantidadLecturas], 5); //guarda la temperatura en un char array
-    hora.toCharArray(hourReadings[cantidadLecturas], 9); //guarda la hora en un char array
-    fecha.toCharArray(dateReadings[cantidadLecturas], 9); //guarda la fecha en un char array
+    String(sensors.getTempCByIndex(0), 2).toCharArray(tempReadings[cantidadLecturas], 5);  //guarda la temperatura en un char array
+    hora.toCharArray(hourReadings[cantidadLecturas], 9);                                   //guarda la hora en un char array
+    fecha.toCharArray(dateReadings[cantidadLecturas], 9);
+    if (gps.location.isValid()) {
+      
+      dtostrf(gps.location.lat(),8,6,latReadings[cantidadLecturas]);
+      dtostrf(gps.location.lng(),10,6,lngReadings[cantidadLecturas]);
+      Serial.println(latReadings[cantidadLecturas]);
+      Serial.println(lngReadings[cantidadLecturas]);
+     
+    }
+    //guarda la fecha en un char array
     cantidadLecturas = cantidadLecturas + 1;
     Serial.println("Temperatura guardada");
     Serial.println(tempReadings[cantidadLecturas - 1]);
@@ -87,6 +129,8 @@ void loop() {
       JsonObject update = updates.createNestedObject();
       update["TimeStamp"] = hourReadings[i - 1];
       update["Temp"] = tempReadings[i - 1];
+      update["Lat"] = latReadings[i - 1];
+      update["Lng"] = lngReadings[i - 1];
       String requestBody;
       serializeJson(json, requestBody);
       Serial.println(requestBody);
